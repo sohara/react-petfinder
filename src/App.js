57fc9e912ad26c62c9f42932b066dc98ed2c53d7
Sean O'Hara <sohara@sohara.com>
2018-11-27 16:47:42 -0500

    Initial commit/project setup

---
+++ src/App.js
@@ -0,0 +1,40 @@
+/* global React ReactDOM */
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Leo",
+        animal: "Cat",
+        breed: "Furry"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
